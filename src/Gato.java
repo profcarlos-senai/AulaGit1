7f3a892c1ae0c4d7fed868d53ad96403823e0d48
Your Name <you@example.com>
2024-10-18 17:24:47 -0300

    sets do gato

--- src/Gato.java
+++ src/Gato.java
@@ -2,4 +2,16 @@ public class Gato {
     private String nome;
     private int patas;
     private String cor;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
