4de740e64fbfec5fd45283c74d2455a244c5cfd7
Your Name <you@example.com>
2024-10-18 17:24:01 -0300

    gets do gato

--- src/Gato.java
+++ src/Gato.java
@@ -2,4 +2,16 @@ public class Gato {
     private String nome;
     private int patas;
     private String cor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
 }
